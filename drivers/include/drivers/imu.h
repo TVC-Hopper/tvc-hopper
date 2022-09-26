3b0c771129ec0aac3df32048a9da41cc8a688935
jsags <saganski@umich.edu>
2022-09-26 15:45:58 -0400

    implemented about 80% of the IMU driver, initialization routines need to be completed, as well as error handling and detection, reformatting etc.

--- drivers/include/drivers/imu.h
+++ drivers/include/drivers/imu.h
@@ -2,204 +2,324 @@
 #define IMU_H
 
 #include <stdint.h>
+#include <stdbool.h>
 
-/*
- * struct to register I2C callbacks
- * set by application
- */
+//////////////////ACCELEROMETER DATA AND ASSOC. FUNCTIONS///////////////////
 
+//Accelerometer specific data
+typedef struct Accelerometer {
+    float LSB;
+    float xComp; 
+    float yComp; 
+    float zComp;
+
+    uint8_t devAddr; //accelerometer device addr
+
+} Accel_t;
+
+//3 axis accelerometer read
+void IMU_getAccReading(IMU_t *imu);
+
+//convert measurement from LSBs to Gs
+float _convertToGs(int16_t reading, float range);
+
+
+///////////accelerometer registers and associated functions
+//acceleromter registers
+typedef enum {
+    ACC_CHIP_ID    = 0x00,
+    ACC_ERR_REG    = 0x02,
+    ACC_STATUS     = 0x03,
+    ACC_X_LSB      = 0x12,
+    ACC_X_MSB      = 0x13,
+    ACC_Y_LSB      = 0x14,
+    ACC_Y_MSB      = 0x15,
+    ACC_Z_LSB      = 0x16,
+    ACC_Z_MSB      = 0x17,
+    SENSORTIME_0   = 0x18,
+    SENSORTIME_1   = 0x19,
+    SENSORTIME_2   = 0x1A,
+    ACC_INT_STAT_1 = 0x1D,
+    TEMP_MSB       = 0x22,
+    TEMP_LSB       = 0x23,
+    FIFO_LENGTH_0  = 0x24,
+    FIFO_LENGTH_1  = 0x25,
+    FIFO_DATA      = 0x26,
+    ACC_CONF       = 0x40,
+    ACC_RANGE      = 0x41,
+    FIFO_DOWNS     = 0x45,
+    FIFO_WTM_0     = 0x46,
+    FIFO_WTM_1     = 0x47,
+    FIFO_CONFIG_0  = 0x48,
+    FIFO_CONFIG_1  = 0x49,
+    INT1_IOCTRL    = 0x53,
+    INT2_IOCTRL    = 0x54,
+    INT_MAP_DATA   = 0x58,
+    ACC_SELF_TEST  = 0x6D,
+    ACC_PWR_CONF   = 0x7C,
+    ACC_PWR_CTRL   = 0x7D,
+    ACC_SOFT_RESET = 0x7E
+} AccReg_t;
+
+//set accelerometer register
+void IMU_setAccReg(IMU_t* imu, AccReg_t regAddr, uint8_t regWriteVal);
+
+//return accelerometer register value
+uint8_t IMU_getAccReg(IMU_t* imu, AccReg_t regAddr);
+
+// get temp reeading (temp registers are in accelerometer)
+void IMU_getTempReading(IMU_t* imu);
+
+//set range
+typedef enum {
+  RANGE_3G  = 0x00, //.0000916 g/LSB
+  RANGE_6G  = 0x01, //.000183  g/LSB
+  RANGE_12G = 0x02, //.000366  g/LSB
+  RANGE_24G = 0x03  //.000632  g/LSB
+} AccRange_t;
+
+void IMU_setAccelRange(IMU_t *imu, AccRange_t range);
+
+//set oversampling ratio and output data rate (implicitly sets LP filter bandwidth)
+typedef enum {
+  OSR4 = 0x08, //4x Nyquist Rate
+  OSR2 = 0x09, //2x Nyquist Rate
+  NONE = 0x0A  //1x Nyquist Rate
+} ACC_OSR_t; 
+
+typedef enum {
+  HZ_12_5 = 0x05,
+  HZ_25   = 0x06,
+  HZ_50   = 0x07,
+  HZ_100  = 0x08,
+  HZ_200  = 0x09,
+  HZ_400  = 0x0A,
+  HZ_800  = 0x0B,
+  HZ_1600 = 0x0C
+} ACC_ODR_t;
+
+void IMU_setAccelOsrOdr(IMU_t *imu, ACC_OSR_t osr, ACC_ODR_t odr);
+
+//Accelerometer device addresses
+typedef enum {
+    SDO1_LOW  = 0,
+    SDO1_HIGH
+} SD01State_t;
+
+
+
+//////////////////GYROSCOPE PARAMETERS AND ASSOC. FUNCTIONS///////////////////
+// Gyroscope specific data
+typedef struct Gyroscope { 
+    float LSB;
+    float xComp; 
+    float yComp; 
+    float zComp;
+    uint8_t devAddr;
+} Gyro_t;
+
+// update x,y,z components of Angular Velocity
+void IMU_getGyroReading(IMU_t *imu);
+
+//convert measurement from LSBs to angular velocity
+float _convertToDegPerSec(uint16_t reading, float LSB);
+
+// Gyroscope register addresses
+typedef enum {
+    GYRO_CHIP_ID      = 0x00,
+    RATE_X_LSB        = 0x02,
+    RATE_X_MSB        = 0x03,
+    RATE_Y_LSB        = 0x04,
+    RATE_Y_MSB        = 0x05,
+    RATE_Z_LSB        = 0x06,
+    RATE_Z_MSB        = 0x07,
+    GYRO_INT_STAT_1   = 0x0A,
+    FIFO_STATUS       = 0x0E,
+    GYRO_RANGE        = 0x0F,
+    GYRO_BANDWIDTH    = 0x10,
+    GYRO_LPM1         = 0x11,
+    GYRO_SOFT_RESET   = 0x14,
+    GYRO_INT_CTRL     = 0x15,
+    INT3_INT4_IO_CONF = 0x16,
+    INT3_INT4_IO_CONF = 0x18,
+    FIFO_WM_EN        = 0x1E,
+    FIFO_EXT_INT_S    = 0x34,
+    GYRO_SELF_TEST    = 0x3C,
+    FIFO_CONFIG_0     = 0x3D,
+    FIFO_CONFIG_1     = 0x3E,
+    FIFO_DATA         = 0x3F
+} GyroReg_t;
+
+//set gyroscope register
+void IMU_setGyroReg(IMU_t *imu, GyroReg_t regAddr, uint8_t regWriteVal);
+
+//return gyro register value
+uint8_t IMU_getGyroReg(IMU_t* imu, GyroReg_t regAddr);
+
+typedef enum {
+  RATE_2000_DS  = 0x00, //.061  deg/sec/LSB
+  RATE_1000_DS  = 0x01, //.0305 deg/sec/LSB
+  RATE_500_DS   = 0x02, //.0153 deg/sec/LSB
+  RATE_250_DS   = 0x03, //.0076 deg/sec/LSB
+  RATE_125_DS   = 0x04  //.0038 deg/sec/LSB
+} GyroRange_t;
+
+//set range
+void IMU_setGyroRange(IMU_t *imu, GyroRange_t range);
+
+//set output data rate (implicitly sets LP filter bandwidth)
+typedef enum {
+    ODR_2000_BW_532 = 0x00,
+    ODR_2000_BW_230 = 0x01,
+    ODR_1000_BW_116 = 0x02,
+    ODR_400_BW_47   = 0x03,
+    ODR_200_BW_23   = 0x04,
+    ODR_100_BW_12   = 0x05,
+    ODR_200_BW_64   = 0x06,
+    ODR_100_BW_32   = 0x07
+} GyroOdrBw_t;
+
+void IMU_setGyroOdrBW(IMU_t *imu, GyroOdrBw_t odrBw);
+
+////////////// IMU OBJECT DATA AND FUNCTIONS ///////////////////////
+
+// struct to register I2C callbacks set by application
 typedef struct IMUcbk {
     
-    int (*onI2Cwrite)(uint16_t addr, uint8_t *bus_data, uint16_t size);
-    int (*onI2Cread)(uint16_t addr, uint8_t *bus_data, uint16_t size);
+    uint8_t (*onI2Cwrite)(uint8_t devAddr, uint8_t *bus_data, uint16_t size);
+    uint8_t (*onI2Cread)(uint8_t devAddr, uint8_t *bus_data, uint16_t size);
 
 } IMUcbk_t;
 
-/* 
- * Accelerometer specific data
- */
-typedef struct Accelerometer {
-    uint8_t AccelAddr; //accelerometer device addr
-    
-    float xComp; 
-    float yComp; 
-    float zComp;
-} Accel_t;
+typedef union rawData{
+    uint8_t bytes[6];
+    int16_t shorts[3];
+}rawData_t;
 
-/* 
- * Accelerometer register addresses
- */
-typedef enum {
-    ACC_X = 0x00,
-    ACC_Y = 0x01,
-    ACC_Z = 0x02
-    /* .
-       .
-       .
-          */
-} AccReg_t;
+typedef enum{
+  ACCEL = 0,
+  GYRO
+}deviceType_t;
 
-/* 
- * Gyroscope specific data
- */
-typedef struct Gyroscope {
-    uint8_t GyroAddr; //gyroscope device addr
+typedef enum{
+  SLAVE_REG_ADDR = 0,
+  SLAVE_REG_VAL
+}addrType_t;
 
-    float xComp; 
-    float yComp; 
-    float zComp;
-} Gyro_t;
-
-/* 
- * Gyroscope register addresses
- */
-typedef enum {
-    GYR_X = 0x00,
-    GYR_Y = 0x01,
-    GYR_Z = 0x02
-    /* .
-       .
-       .
-          */
-} GyroReg_t;
-
-/* 
- * Magnetometer specific data
- */
-typedef struct Magnetometer {
-    uint8_t MagAddr; //Magnetometer device addr
-    
-    float xComp; 
-    float yComp; 
-    float zCOmp;
-} Mag_t;
-
-/* 
- * Magnetomoeter register addresses
- */
-typedef enum {
-    MAG_X = 0x00,
-    MAG_Y = 0x01,
-    MAG_Z = 0x02
-    /* .
-       .
-       .
-          */
-} MagReg_t;
-
-/* 
- * Temperature Sensor specific data
- */
-typedef struct TempSensor {
-    uint8_t TempSenseAddr; //Temperature sensor device addr
-    
-    float degK;
-    
-} TempSense_t;
-
-/* 
- * Temperature register addresses
- */
-typedef enum {
-    TEMP_K = 0x00,
-    /* .
-       .
-       .
-          */
-} TempReg_t;
-;
-/* 
- * IMU data
- */
 typedef struct IMU {
-    
     /* Callbacks */
     IMUcbk_t i2cCallBacks;
     
     /* Sensor Data */
     Accel_t Acc;
     Gyro_t Gyro;
-    Mag_t Mag;
-    TempSense_t Tsense;
+    float degC;
 
+    /*Rx Tx buffer*/
+    rawData_t _RxTxData;
+    
+    /* I2C params */
+    uint16_t _msgLen;
+    uint8_t _curI2CdevAddr;
 } IMU_t;
 
-/////////////////// IMU Functions ////////////////////
+/////////////////// IMU initialization Functions and Parameters////////////////////////////
+typedef struct ImuInit{
+  SD01State_t sd01;
+}ImuInit_t;
+typedef enum{
+  PUSH_PULL, 
+  OPEN_DRAIN
+} IntPpOd_t;
 
-/*
- * intialize the IMU
- */
-int IMU_init(IMU_t *imu /* other args TBD */);
+typedef enum{
+  ACT_HIGH, 
+  ACT_LOW
+} IntLvl_t;
 
+typedef enum{
+  INT1, 
+  INT2
+} AccIntPins_t;
 
-/////////////// Accelerometer Functions ///////////////
+typedef enum{
+  INT3, 
+  INT4
+} GyroIntPins_t;
 
-/* 
- * update x,y,z components of acceleration
- */
-void IMU_getAccReading(IMU_t *imu);
+//Accelerometer initialization parameters 
+typedef struct AccelInit{
+  AccRange_t range;
+  ACC_OSR_t osr; 
+  ACC_ODR_t odr;
+  AccelInt_t AccInts;
+} AccelInit_t;
 
-/* 
- * write data to accelerometer reg
- */
-int IMU_writeAccReg(IMU_t *imu, uint8_t reg, AccReg_t data);
+//intialize accelerometer
+void _IMU_AccInit(IMU_t* imu, AccelInit_t* acc);
 
-/* 
- * read data from accelerometer reg
- */
-uint8_t IMU_readAccReg(IMU_t *imu, AccReg_t reg);
+//configure accelerometer interrupts
+typedef struct AccelInt{
+  AccIntPins_t IntPin;
+  bool pinIsInput;
+  uint8_t PpOd;
+  uint8_t activeLvl;
+} AccelInt_t;
 
-///////////////// Gyroscope Functions /////////////////
+//configure accelerometer interrupts
+void _IMU_AccIntConfig(IMU_t* imu, AccelInt_t* acc);
 
-/* 
- * update x,y,z components of Angular Velocity
- */
-void IMU_getGyroReading(IMU_t *imu);
+//gyroscope initialization parameters
+typedef struct GyroInit{
+  GyroRange_t range;
+  GyroOdrBw_t OdrBw;
+  AccelInt_t GyroInts;
+} GyroInit_t;
 
-/* 
- * write data to gyroscope reg
- */
-int IMU_writeGyroReg(IMU_t *imu, uint8_t reg, GyroReg_t data);
+//intialize gyroscope
+void _IMU_GyroInit(IMU_t* imu, GyroInit_t* gyro);
 
-/* 
- * read data from gyroscope reg
- */
-uint8_t IMU_readGyroReg(IMU_t *imu, uint8_t reg);
+//gyroscope interrupt initialization parameters
+typedef struct GyroInt{
+  GyroIntPins_t IntPin;
+  bool pinIsInput;
+  uint8_t PpOd;
+  uint8_t activeLvl;
+} GyroInt_t;
 
-//////////////// Magnetometer Functions ////////////////
+//configure gyroscope interrupts
+void _IMU_GyroIntConfig(IMU_t* imu, GyroInt_t* acc);
 
-/* 
- * update x,y,z components of magnetic field
- */
-void IMU_getMagReading(IMU_t *imu);
+/////////////////// IMU Private Functions ////////////////////
 
-/* 
- * write data to gyroscope reg
- */
-int IMU_writeMagReg(IMU_t *imu, MagReg_t reg, uint8_t data);
+//convert raw data measurement to LSBs
+void _processRawData(rawData_t *reading);
 
-/* 
- * read data from gyroscope reg
- */
-uint8_t IMU_readMagReg(IMU_t *imu, MagReg_t reg);
+//set imu register
+uint8_t _IMU_setReg(IMU_t *imu, deviceType_t dev, uint8_t regAddr, uint8_t regWriteVal);
 
-///////////// Temperature Sensor Functions //////////////
+//write IMU register
+uint8_t _IMU_writeReg(IMU_t *imu, deviceType_t dev, uint8_t regAddr, uint8_t regWriteVal);
 
-/* 
- * update x,y,z components of magnetic field
- */
-void IMU_getTempReading(IMU_t *imu);
+//Read IMU register
+uint8_t _IMU_readReg(IMU_t *imu, deviceType_t dev, uint8_t regAddr, uint16_t msgLen);
 
-/* 
- * write data to gyroscope reg
- */
-int IMU_writeTempReg(IMU_t *imu, TempReg_t reg, uint8_t data);
+//set device address for I2C
+void _IMU_setDevAddr(IMU_t *imu, deviceType_t dev);
 
-/* 
- * read data from gyroscope reg
- */
-uint8_t IMU_readTempReg(IMU_t *imu, TempReg_t reg);
+//set message length for I2C
+void _IMU_setMsgLen(IMU_t* imu, uint16_t msgLen);
 
+//set register address for I2C
+void _IMU_setRegAddr(IMU_t *imu, uint8_t regAddr);
 
+//set register write value for I2C
+void _IMU_setRegVal(IMU_t *imu, uint8_t regWriteVal);
+
+//invoke write callback
+uint8_t _IMU_write(IMU_t *imu);
+
+//invoke read callback
+uint8_t _IMU_read(IMU_t *imu);
 
 #endif
--- drivers/src/imu.c
+++ drivers/src/imu.c
@@ -1,78 +1,187 @@
 #include "imu.h"
 #include <stdint.h>
+#include <stdbool.h>
 
-/////////////////// IMU Functions ////////////////////
+#define SIZE_3AXIS_READ 6
+#define I2C_READ_FAIL 1
+#define I2C_WRITE_FAIL 2
 
-/*
- * intialize the IMU
- */
-int IMU_init(IMU_t *imu /* other args TBD */){return 0;}
-
+/////////////////////////Initialize IMU /////////////////////////////
+int IMU_init(IMU_t* imu, AccelInit_t* Acc, GyroInit_t* gyro){return 0;}
 
 /////////////// Accelerometer Functions ///////////////
 
-/* 
- * update x,y,z components of acceleration
- */
-void IMU_getAccReading(IMU_t *imu){return;}
+//set accelerometer register
+void IMU_setAccReg(IMU_t* imu, AccReg_t regAddr, uint8_t regWriteVal){
+  _IMU_setReg(imu, ACCEL, regAddr, regWriteVal);
+}
 
-/* 
- * write data to accelerometer reg
- */
-int IMU_writeAccReg(IMU_t *imu, uint8_t reg, AccReg_t data){return 0;}
+//return accelerometer register value
+uint8_t IMU_getAccReg(IMU_t* imu, AccReg_t regAddr){
+  _IMU_readReg(imu, ACCEL, regAddr, 1);
+  return imu->_RxTxData.bytes[0];
+}
 
-/* 
- * read data from accelerometer reg
- */
-uint8_t IMU_readAccReg(IMU_t *imu, AccReg_t reg){return 0;}
+// set range
+void IMU_setAccelRange(IMU_t *imu, AccRange_t range){
+  IMU_setAccReg(imu, ACC_RANGE, range);
+  imu->Acc.LSB = ((2 << (range + 1)) * 1.5) / 32768.0;
+}
+
+// set over sampling ratio, output datarate
+void IMU_setAccelOsrOdr(IMU_t *imu, ACC_OSR_t osr, ACC_ODR_t odr){
+  uint8_t writeVal = (osr << 4) | odr;
+  IMU_setAccReg(imu, ACC_CONF, writeVal);
+}
+
+// update x,y,z components of acceleration
+void IMU_getAccReading(IMU_t *imu){
+  _IMU_readReg(imu, ACCEL, ACC_X_LSB, SIZE_3AXIS_READ);
+  _processRawData(&(imu->_RxTxData));
+  imu->Acc.xComp = _convertToGs(imu->_RxTxData.shorts[0], imu->Acc.LSB);
+  imu->Acc.yComp = _convertToGs(imu->_RxTxData.shorts[1], imu->Acc.LSB);
+  imu->Acc.zComp = _convertToGs(imu->_RxTxData.shorts[2], imu->Acc.LSB);
+}
+
+//convert measurement from LSBs to Gs
+float _convertToGs(int16_t reading, float LSB){
+  return reading * LSB;
+}
+
+//intialize accelerometer
+void _IMU_AccInit(IMU_t* imu, AccelInit_t* acc){
+  IMU_setAccelRange(imu, acc->range);
+  IMU_setAccelOsrOdr(imu, acc->osr, acc->odr);  
+  return;
+}
+
+//configure accelerometer interrupts
+void _IMU_AccIntConfig(IMU_t* imu, AccelInt_t* acc){ return;}
+
+///////////// Temperature Sensor Functions //////////////
+#define SIZE_TEMP_READ 2
+void IMU_getTempReading(IMU_t* imu){
+    _IMU_readReg(imu, ACCEL, TEMP_MSB, SIZE_TEMP_READ);
+    int16_t upper_8bits = (int8_t)imu->_RxTxData.bytes[0];
+    int16_t lower_3bits = (int8_t)imu->_RxTxData.bytes[1];
+    int16_t tempDataLsb = (upper_8bits << 3) | lower_3bits;
+    imu->degC = tempDataLsb * .125 + 23;
+}
 
 ///////////////// Gyroscope Functions /////////////////
 
-/* 
- * update x,y,z components of Angular Velocity
- */
-void IMU_getGyroReading(IMU_t *imu){return;}
+//set gyro register
+void IMU_setGyroReg(IMU_t *imu, GyroReg_t regAddr, uint8_t regWriteVal){
+  _IMU_setReg(imu, GYRO, regAddr, regWriteVal);
+}
 
-/* 
- * write data to gyroscope reg
- */
-int IMU_writeGyroReg(IMU_t *imu, uint8_t reg, GyroReg_t data){return 0;}
+//return gyro register value
+uint8_t IMU_getGyroReg(IMU_t* imu, GyroReg_t regAddr){
+  _IMU_readReg(imu, GYRO, regAddr, 1);
+  return imu->_RxTxData.bytes[0];
+}
 
-/* 
- * read data from gyroscope reg
- */
-uint8_t IMU_readGyroReg(IMU_t *imu, uint8_t reg){return 0;}
+// set range
+void IMU_setGyroRange(IMU_t *imu, GyroRange_t range){
+  IMU_setGyroReg(imu, GYRO_RANGE, range);
+  imu->Gyro.LSB = (2000.0 / (2 << range)) / 32767.0;
+}
 
-//////////////// Magnetometer Functions ////////////////
+// set over sampling ratio, output datarate
+void IMU_setGyroOdrBW(IMU_t *imu, GyroOdrBw_t odrBw){
+  IMU_setGyroReg(imu, GYRO_BANDWIDTH, odrBw);
+}
 
-/* 
- * update x,y,z components of magnetic field
- */
-void IMU_getMagReading(IMU_t *imu){return;}
+// update x,y,z components of Angular Velocity
+void IMU_getGyroReading(IMU_t *imu){
+  _IMU_readReg(imu, GYRO, RATE_X_LSB, SIZE_3AXIS_READ);
+  _processRawData(&(imu->_RxTxData));
+  imu->Gyro.xComp = _convertToDegPerSec(imu->_RxTxData.shorts[0], imu->Gyro.LSB);
+  imu->Gyro.yComp = _convertToDegPerSec(imu->_RxTxData.shorts[1], imu->Gyro.LSB);
+  imu->Gyro.zComp = _convertToDegPerSec(imu->_RxTxData.shorts[2], imu->Gyro.LSB);
+}
 
-/* 
- * write data to gyroscope reg
- */
-int IMU_writeMagReg(IMU_t *imu, MagReg_t reg, uint8_t data){return 0;}
+//convert measurement from LSBs to angular velocity
+float _convertToDegPerSec(uint16_t reading, float LSB){
+  return reading * LSB;
+}
 
-/* 
- * read data from gyroscope reg
- */
-uint8_t IMU_readMagReg(IMU_t *imu, MagReg_t reg){return 0;}
+//intialize gyroscope
+void _IMU_GyroInit(IMU_t* imu, GyroInit_t* gyro){
+  IMU_setGyroRange(imu, gyro->range);
+  IMU_setGyroOdrBW(imu, gyro->OdrBw);
+  return;
+}
 
-///////////// Temperature Sensor Functions //////////////
+//configure gyroscope interrupts
+void _IMU_GyroIntConfig(IMU_t* imu, GyroInt_t* acc){ return;}
 
-/* 
- * update x,y,z components of magnetic field
- */
-void IMU_getTempReading(IMU_t *imu){return;}
+/////////////////////////private general IMU implementation functions///////////////////////
+#define REG_SEL_MSG_LEN 1
+#define WRITE_REG_MSG_LEN 2
 
-/* 
- * write data to gyroscope reg
- */
-int IMU_writeTempReg(IMU_t *imu, TempReg_t reg, uint8_t data){return 0;}
+//convert raw data measurement to LSBs
+void _processRawData(rawData_t *reading){
+  for (int i = 0; i < 3; ++i){
+    reading->shorts[i] = (reading->bytes[i + 1] << 8) | reading->bytes[i];
+  }
+}
 
-/* 
- * read data from gyroscope reg
- */
-uint8_t IMU_readTempReg(IMU_t *imu, TempReg_t reg){return 0;}
+//set imu register
+uint8_t _IMU_setReg(IMU_t *imu, deviceType_t dev, uint8_t regAddr, uint8_t regWriteVal){
+  _IMU_readReg(imu, dev, regAddr, 1);
+  uint8_t regReadVal = imu->_RxTxData.bytes[0];
+  regReadVal &= ~regWriteVal;
+  regWriteVal |= regReadVal;
+  _IMU_writeReg(imu, dev, regAddr, regWriteVal);
+}
+
+//write IMU register
+uint8_t _IMU_writeReg(IMU_t *imu, deviceType_t dev, uint8_t regAddr, uint8_t regWriteVal){
+  _IMU_setMsgLen(imu, WRITE_REG_MSG_LEN);
+  _IMU_setDevAddr(imu, dev);
+  _IMU_setRegAddr(imu, regAddr);
+  _IMU_setRegVal(imu, regWriteVal);
+  return _IMU_write(imu);
+}
+
+//Read IMU register
+uint8_t _IMU_readReg(IMU_t *imu, deviceType_t dev, uint8_t regAddr, uint16_t msgLen){
+  _IMU_setMsgLen(imu, REG_SEL_MSG_LEN);
+  _IMU_setDevAddr(imu, dev);
+  _IMU_setRegAddr(imu, regAddr);
+  _IMU_write(imu);
+  _IMU_setMsgLen(imu, msgLen);
+  return _IMU_read(imu);
+}
+
+//set device address for I2C
+void _IMU_setDevAddr(IMU_t *imu, deviceType_t dev){
+  if (dev == ACCEL){imu->_curI2CdevAddr = imu->Acc.devAddr;}
+  else {imu->_curI2CdevAddr = imu->Gyro.devAddr;}
+}
+
+//set message length for I2C
+void _IMU_setMsgLen(IMU_t* imu, uint16_t msgLen){
+  imu->_msgLen = msgLen;
+}
+
+//set register address for I2C
+void _IMU_setRegAddr(IMU_t *imu, uint8_t regAddr){
+  imu->_RxTxData.bytes[SLAVE_REG_ADDR] = regAddr;
+}
+
+//set register write value for I2C
+void _IMU_setRegVal(IMU_t *imu, uint8_t regWriteVal){
+  imu->_RxTxData.bytes[SLAVE_REG_VAL] = regWriteVal;
+}
+
+//invoke write callback
+uint8_t _IMU_write(IMU_t *imu){
+  return imu->i2cCallBacks.onI2Cwrite(imu->_curI2CdevAddr, imu->_RxTxData.bytes, imu->_msgLen);  
+}
+
+//invoke read callback
+uint8_t _IMU_read(IMU_t *imu){
+  return imu->i2cCallBacks.onI2Cread(imu->_curI2CdevAddr, imu->_RxTxData.bytes, imu->_msgLen);
+}
