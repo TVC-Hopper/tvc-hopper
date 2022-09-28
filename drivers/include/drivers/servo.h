261f62555f7ecd7eae35477020352bfdcce4dc74
jsags <saganski@umich.edu>
2022-09-28 16:35:09 -0400

    added servo driver

--- drivers/include/drivers/servo.h
+++ drivers/include/drivers/servo.h
@@ -2,34 +2,136 @@
 #define SERVO_H
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+#define BAD_MIN_MAX_THRESH 1
+#define DEGREES_MIN 0
+#define DEGREES_MAX 180
+
+/**
+ * @brief Servo Initialization parameters
+ *  pulseWidth0deg_us : pulsewidth in microseconds corresponding to 0 deg
+ *  pulseWidth180deg_us : pulsewidth in microseconds corresponding to 180 deg
+ *  deadband : half deadband width
+ *  posStart_deg : servo start position in degrees
+ *  applyDB : apply deadband compensation
+ */
+typedef struct ServoInit{
+
+    uint32_t pulseWidth0deg_us;
+    uint32_t pulseWidth180deg_us;
+    uint8_t deadband;
+    uint8_t posStart_deg;
+    bool applyDB;
+
+}ServoInit_t;
+
+/**
+ * @brief Servo callbacks
+ * onNewDutyCycle : set pulsewidth in microseconds
+ */
 typedef struct ServoCallbacks_s {
-    
-    void (*onNewDutyCycle)(uint16_t pulseWidth_us);
+
+    void (*onNewDutyCycle)(uint32_t pulseWidth_us);
 
 } ServoCallbacks_t;
 
-
+/**
+ * @brief Servo driver object
+ *  callbacks : callback to PWM write method
+ *  lastWrite_us : last commanded position in micros
+ *  lastWrite_deg : last commanded position in degrees
+ *  pulseWidth0deg_us : pulsewidth in microseconds corresponding to 0 deg
+ *  pulseWidth180deg_us : pulsewidth in microseconds corresponding to 180 deg
+ *  deadBand_us : half deadband width
+ *  degToMicros : conversion between degrees and micros
+ *  microsToDeg : conversion between micros and degrees
+ *  writeMicros : function pointer to microsecond write method
+ *                write with or without deadband compensation
+ */
 typedef struct Servo_s {
     
     ServoCallbacks_t callbacks;
 
-    uint16_t lastWrite_us;
+    uint32_t lastWrite_us;
     uint8_t lastWrite_deg;
 
-    uint16_t pulseWidth_min;
-    uint16_t pulseWidth_max;
+    uint32_t pulseWidthMin_us;
+    uint32_t pulseWidthMax_us;
+    uint8_t deadBand_us;
+
+    float degToMicros;
+    float microsToDeg;
+
+    uint32_t (*writeMicros)(Servo_t* Servo, uint32_t micros);
 
 } Servo_t;
 
-void Servo_Init(Servo_t* Servo, uint16_t pulseWidth0deg_us, uint16_t pulseWidth180deg_us);
+/**
+ * @brief Initializes Servo
+ * 
+ * @param Servo : ptr to object of type Servo_t
+ * @param ServoInit : servo initialization structure
+ * @return uint8_t : 0 -> no errors, 1 -> init failed
+ */
+uint8_t Servo_Init(Servo_t* Servo, ServoInit_t* ServoInit);
 
-uint16_t Servo_GetLastWriteDeg(Servo_t *Servo);
+/**
+ * @brief return current position in degrees
+ * 
+ * @param Servo ptr to object of type Servo_t
+ * @return uint8_t : degrees between 0 and 180
+ */
+uint8_t Servo_GetLastWriteDeg(Servo_t* Servo);
 
-uint16_t Servo_GetLastWriteMicros(Servo_t *Servo);
+/**
+ * @brief return current position in micros
+ * 
+ * @param Servo ptr to object of type Servo_t
+ * @return uint32_t 
+ */
+uint32_t Servo_GetLastWriteMicros(Servo_t* Servo);
 
-uint16_t Servo_WritePosDeg(Servo_t *Servo, uint8_t deg);
+/**
+ * @brief write new position in degrees
+ * 
+ * @param Servo ptr to object of type Servo_t
+ * @param deg position commanded in degrees
+ */
+void Servo_WritePosDeg(Servo_t *Servo, uint8_t deg);
 
-uint16_t Servo_WritePosMicros(Servo_t *Servo, uint16_t micros);
+/**
+ * @brief write new position in micros
+ * 
+ * @param Servo ptr to object of type Servo_t
+ * @param micros position commanded in microseconds
+ */
+void Servo_WritePosMicros(Servo_t *Servo, uint32_t micros);
+
+/**
+ * @brief compensate for servo deadband
+ * 
+ * @param Servo ptr to object of type Servo_t
+ * @param micros : position commanded in microseconds
+ * @return uint32_t : returns deadband compensated position in microseconds
+ */
+uint32_t applyDeadband(Servo_t* Servo, uint32_t micros);
+
+/**
+ * @brief write new position with deadband compensation
+ * 
+ * @param Servo ptr to object of type Servo_t
+ * @param micros position commanded in microseconds
+ */
+void Servo_WriteMicrosDB(Servo_t* Servo, uint32_t micros);
+
+/**
+ * @brief write new position without deadband compensation
+ * 
+ * @param Servo ptr to object of type Servo_t
+ * @param micros position commanded in microseconds
+ */
+void Servo_WriteMicrosNoDB(Servo_t* Servo, uint32_t micros);
 
 #endif
--- drivers/src/servo.c
+++ drivers/src/servo.c
@@ -1,12 +1,76 @@
 #include "servo.h"
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-void Servo_Init(Servo_t* Servo, uint16_t pulseWidth0deg_us, uint16_t pulseWidth180deg_us){return;}
+uint8_t Servo_Init(Servo_t* Servo, ServoInit_t* ServoInit){
+    if (ServoInit->pulseWidth0deg_us < ServoInit->pulseWidth180deg_us) {return BAD_MIN_MAX_THRESH;}
+    float width = ServoInit->pulseWidth0deg_us - ServoInit->pulseWidth180deg_us;
+    Servo->pulseWidthMin_us = ServoInit->pulseWidth0deg_us;
+    Servo->pulseWidthMax_us = ServoInit->pulseWidth180deg_us;
+    Servo->degToMicros = width / DEGREES_MAX;
+    Servo->microsToDeg = 1 / Servo->degToMicros;
+    if (ServoInit->applyDB){Servo->writeMicros = &Servo_WriteMicrosDB;}
+    else {Servo->writeMicros = &Servo_WriteMicrosNoDB;}
+    Servo_WritePosDeg(Servo, ServoInit->posStart_deg);
 
-uint16_t Servo_GetLastWriteDeg(Servo_t *Servo){return 0;}
+}
 
-uint16_t Servo_GetLastWriteMicros(Servo_t *Servo){return 0;}
+uint8_t Servo_GetLastWriteDeg(Servo_t* Servo){return Servo->lastWrite_deg;}
 
-uint16_t Servo_WritePosDeg(Servo_t *Servo, uint8_t deg){return 0;}
+uint32_t Servo_GetLastWriteMicros(Servo_t* Servo){return Servo->lastWrite_us;}
 
-uint16_t Servo_WritePosMicros(Servo_t *Servo, uint16_t micros){return 0;}
+uint32_t Servo_applyDeadband(Servo_t* Servo, uint32_t micros){
+    if (abs((int32_t)micros - (int32_t)Servo->lastWrite_us) < Servo->deadBand_us) {}
+    else if (micros > Servo->lastWrite_us && 
+             micros < (Servo->pulseWidthMax_us - Servo->deadBand_us)){
+                micros += Servo->deadBand_us;
+    }
+    else if (micros < Servo->lastWrite_us && 
+             micros > (Servo->pulseWidthMin_us + Servo->deadBand_us)){
+                micros -= Servo->deadBand_us;
+             }
+    else {}
+    return micros;
+}
+
+void Servo_WritePosDeg(Servo_t *Servo, uint8_t deg){
+    uint32_t micros = 0;
+    if (deg < DEGREES_MIN){ 
+        micros = Servo->pulseWidthMin_us;
+        deg = DEGREES_MIN;    
+    }
+    else if (deg > DEGREES_MAX){ 
+        micros = Servo->pulseWidthMax_us;
+        deg = DEGREES_MAX; 
+    }
+    else {micros = Servo->pulseWidthMin_us + Servo->degToMicros * deg;}
+    Servo->lastWrite_deg = deg;
+    Servo->lastWrite_us = micros;
+    Servo->writeMicros(Servo, micros);
+}
+
+void Servo_WritePosMicros(Servo_t *Servo, uint32_t micros){
+    if (micros < Servo->pulseWidthMin_us){ 
+        micros = Servo->pulseWidthMin_us;
+        Servo->lastWrite_deg = DEGREES_MIN;    
+    }
+    else if (micros > Servo->pulseWidthMax_us){ 
+        micros = Servo->pulseWidthMax_us;
+        Servo->lastWrite_deg = DEGREES_MAX; 
+    }
+    else {micros = Servo->microsToDeg * micros;}
+    Servo->lastWrite_deg = micros * Servo->microsToDeg;
+    Servo->lastWrite_us = micros;
+    Servo->writeMicros(Servo, micros);
+}
+
+
+void Servo_WriteMicrosDB(Servo_t* Servo, uint32_t micros){
+    micros = applyDeadband(Servo, micros);
+    Servo->callbacks.onNewDutyCycle(micros);
+}
+
+void Servo_WriteMicrosNoDB(Servo_t* Servo, uint32_t micros){
+    Servo->callbacks.onNewDutyCycle(micros);
+}
