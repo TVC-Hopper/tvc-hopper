4775ae7698f316426fc5bb322b134b167fce6175
Tejal Mahajan <78976839+t-mahajan@users.noreply.github.com>
2022-10-13 10:59:59 -0400

    Add files via upload

---
+++ simulink/nonlinear_function_call.m
@@ -0,0 +1,48 @@
+function [sys,x0,str,ts] = nonlinear_function_call(t,x,u,flag)
+
+switch flag
+  %%%%%%%%%%%%%%%%%%
+  % Initialization %
+  %%%%%%%%%%%%%%%%%%
+  case 0
+    % -CHANGED for C code generation-
+%     s = struct('NumContStates', 12, 'NumDiscStates', 0, 'NumOutputs', 12, 'NumInputs', 5, 'DirFeedthrough', 0, 'NumSampleTimes', 1);
+    s = simsizes;
+        s.NumContStates  = 12;  % Number of states
+        s.NumDiscStates  = 0;   % Number of discrete states
+        s.NumOutputs     = 12;  % Number of outputs
+        s.NumInputs      = 5;   % Number of inputs
+        s.DirFeedthrough = 0;
+        s.NumSampleTimes = 1;
+
+    sys = simsizes(s);  % Initial system -CHANGED for C code generation-
+%     sys = s;
+    x0  = zeros(12,1);   % Initial conditions
+    str = [];           % State ordering strings
+    ts  = [0 0];        % An m-by-2 matrix containing the sample time (period, offset) information
+
+  %%%%%%%%%%%%%%%
+  % Derivatives %
+  %%%%%%%%%%%%%%%
+  case 1
+    sys = nonlinear_dynamics(t,x,u);
+
+  %%%%%%%%%%%
+  % Outputs %
+  %%%%%%%%%%%
+  case 3
+    sys = x; % The outputs are just the states
+
+  %%%%%%%%%%%%%%%%%%%
+  % Unhandled flags %
+  %%%%%%%%%%%%%%%%%%%
+  case { 2, 4, 9 }
+    sys = [];
+
+  %%%%%%%%%%%%%%%%%%%%
+  % Unexpected flags %
+  %%%%%%%%%%%%%%%%%%%%
+  otherwise
+    DAStudio.error('Simulink:blocks:unhandledFlag', num2str(flag));
+
+end
