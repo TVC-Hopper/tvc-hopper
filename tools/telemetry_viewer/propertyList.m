a143a19e3dd3b3fabc9ef75abf33e9786a78bec5
jsags <saganski@umich.edu>
2022-12-06 20:38:43 -0500

    add properties to group

--- tools/telemetry_viewer/propertyList.m
+++ tools/telemetry_viewer/propertyList.m
@@ -18,7 +18,7 @@ classdef propertyList
 
     groups = [{"Command and Control", [1,2,3,4,5]};...
               {"Sensors and Actuators", [6,7,8,9,10, 11]};...
-              {"Tune Control Parameters", [12, 13, 14, 15, 16]}];
+              {"Tune Control Parameters", [12, 13, 14, 15, 16, 17, 18]}];
     numGroups
 
     end
